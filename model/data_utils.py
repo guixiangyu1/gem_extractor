aee78b19f1dbcca496c4df1e2663ef6bf5084ea2
guixiangyu <guixiangyu1@gmail.com>
2019-09-08 17:50:25 +0800

    init

--- model/data_utils.py
+++ model/data_utils.py
@@ -396,7 +396,7 @@ def get_chunks(seq, tags):
         result = [("PER", 0, 2), ("LOC", 3, 4)]
 
     """
-    default = tags[NONE]
+    # default = tags[NONE]
     idx_to_tag = {idx: tag for tag, idx in tags.items()}
     chunks = []
     chunk_start = None
